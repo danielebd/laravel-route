01d9ac94bc8f5f35f273030cb6020d6bad58fea7
danielebd <danielebalebalestro@gmail.com>
2023-11-20 15:11:22 +0100

    db.php

---
+++ config/db.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    [
+        'titolo' => 'Batman: Year One',
+        'data_di_rilascio' => '1987-02-01',
+        'pagine' => 96,
+        'autore' => 'Frank Miller',
+        'artista' => 'David Mazzucchelli',
+    ],
+    [
+        'titolo' => 'The Dark Knight Returns',
+        'data_di_rilascio' => '1986-05-01',
+        'pagine' => 196,
+        'autore' => 'Frank Miller',
+        'artista' => 'Klaus Janson',
+    ],
+    [
+        'titolo' => 'Wonder Woman: The Hiketeia',
+        'data_di_rilascio' => '2002-03-01',
+        'pagine' => 96,
+        'autore' => 'Greg Rucka',
+        'artista' => 'J.G. Jones',
+    ],
+    [
+        'titolo' => 'Superman: Red Son',
+        'data_di_rilascio' => '2003-04-01',
+        'pagine' => 160,
+        'autore' => 'Mark Millar',
+        'artista' => 'Dave Johnson',
+    ],
+    [
+        'titolo' => 'Green Lantern: Rebirth',
+        'data_di_rilascio' => '2004-12-01',
+        'pagine' => 144,
+        'autore' => 'Geoff Johns',
+        'artista' => 'Ethan Van Sciver',
+    ],
+    [
+        'titolo' => 'The Flash: Flashpoint',
+        'data_di_rilascio' => '2011-05-01',
+        'pagine' => 176,
+        'autore' => 'Geoff Johns',
+        'artista' => 'Andy Kubert',
+    ],
+    [
+        'titolo' => 'Aquaman: The Trench',
+        'data_di_rilascio' => '2011-12-01',
+        'pagine' => 144,
+        'autore' => 'Geoff Johns',
+        'artista' => 'Ivan Reis',
+    ],
+    [
+        'titolo' => 'Justice League: Throne of Atlantis',
+        'data_di_rilascio' => '2012-11-01',
+        'pagine' => 192,
+        'autore' => 'Geoff Johns',
+        'artista' => 'Ivan Reis',
+    ],
+    [
+        'titolo' => 'Batman: The Court of Owls',
+        'data_di_rilascio' => '2012-05-01',
+        'pagine' => 176,
+        'autore' => 'Scott Snyder',
+        'artista' => 'Greg Capullo',
+    ],
+    [
+        'titolo' => 'Harley Quinn: Mad Love',
+        'data_di_rilascio' => '1994-02-01',
+        'pagine' => 64,
+        'autore' => 'Paul Dini',
+        'artista' => 'Bruce Timm',
+    ],
+];
+
